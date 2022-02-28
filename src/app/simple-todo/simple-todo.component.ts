cf7e37a11a2e7fb5df2bcb1a4aa5157ea7bfbd85
Roberto Augusto <roberto.augusto@krooze.com.br>
2022-02-27 22:49:54 -0300

    Adding functionality to save task in localStorage

--- src/app/simple-todo/simple-todo.component.ts
+++ src/app/simple-todo/simple-todo.component.ts
@@ -1,6 +1,8 @@
 import { Component, ElementRef, OnInit, QueryList, ViewChildren } from '@angular/core';
 import { Tasks } from '../models/tasks';
 import { TabType } from '../models/simple-todo';
+import { LocalStorageService } from '../services/local-storage.service';
+import { Config } from '../models/config';
 
 @Component({
   selector: 'app-simple-todo',
@@ -10,31 +12,24 @@ export class SimpleTodoComponent implements OnInit {
   @ViewChildren('tab') tabs?: QueryList<ElementRef<HTMLButtonElement>>;
 
   public tabTypeEnum = TabType;
-  theme: boolean = true;
+  config: Config = {
+    isThemeDark: true
+  }
   tabActive: number = this.tabTypeEnum.ALL;
-  tasks: Tasks[] = [
-    {
-      id: 1,
-      title: 'Teste',
-      isComplete: false,
-    },
-    {
-      id: 2,
-      title: 'Teste2',
-      isComplete: true,
-    },
-    {
-      id: 3,
-      title: 'Teste3',
-      isComplete: false,
-    }
-  ];
+  tasks: Tasks[] = [];
 
   constructor(
+    private localStorageService: LocalStorageService,
   ) { }
 
   ngOnInit(): void {
+    this.config = this.localStorageService.getConfig();
+    this.tasks = this.localStorageService.getTask();
+  }
 
+  changeTheme(): void {
+    this.config.isThemeDark = !this.config.isThemeDark;
+    this.localStorageService.setConfig(this.config);
   }
 
   changeTab(element: HTMLButtonElement, tabNumber: TabType): void {
@@ -48,7 +43,7 @@ export class SimpleTodoComponent implements OnInit {
 
   addTask(input: HTMLInputElement): void {
     if (input.value.trim() !== '') {
-      const newId = this.tasks.length <= 0 ? 1 : this.tasks[this.tasks.length - 1].id;
+      const newId = this.tasks.length <= 0 ? 1 : this.tasks[this.tasks.length - 1].id + 1;
 
       const newTask = {
         id: newId,
@@ -57,17 +52,20 @@ export class SimpleTodoComponent implements OnInit {
       }
 
       this.tasks.push(newTask);
+      this.localStorageService.setTask(this.tasks);
+      input.value = '';
     }
   }
 
   changeTask(task: Tasks): void {
     const indexTask = this.tasks.indexOf(task);
     this.tasks[indexTask].isComplete = !this.tasks[indexTask].isComplete;
-    console.log(this.tasks[this.tasks.length - 1].id++);
+    this.localStorageService.setTask(this.tasks);
   }
 
   deleteTask(task: Tasks): void {
     this.tasks = this.tasks.filter(talk => talk.id !== task.id);
+    this.localStorageService.setTask(this.tasks);
   }
 }
 
